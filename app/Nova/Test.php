4077c7fa706369f927913688f2eeadd2b174340e
Jonhathan Rolando “elefanteweb” Rodas Lopez <jrodas4044@gmail.com>
2020-10-04 17:59:04 -0600

    hidden activities tools

--- app/Nova/Test.php
+++ app/Nova/Test.php
@@ -11,6 +11,13 @@ use Laravel\Nova\Http\Requests\NovaRequest;
 class Test extends Resource
 {
     /**
+     * Indicates if the resource should be displayed in the sidebar.
+     *
+     * @var bool
+     */
+    public static $displayInNavigation = false;
+
+    /**
      * The model the resource corresponds to.
      *
      * @var string
