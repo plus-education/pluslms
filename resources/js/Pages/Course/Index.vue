ce77cc6d56789f5303be68e8b574734ba21f1b12
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2021-07-16 11:27:07 -0600

    add clas link button to courses in student view

--- resources/js/Pages/Course/Index.vue
+++ resources/js/Pages/Course/Index.vue
@@ -7,7 +7,7 @@
         </template>
 
         <section class="container m-auto mt-6  md:px-0 sm:px-4">
-            <header class="flex items-center shadow rounded-lg p-8 md:h-48 sm:h-32  bg-cover"
+            <header class=" shadow rounded-lg p-8 md:h-48 sm:h-32  bg-cover"
                 :style="`background-image: url('${course.coverPath}')`"
             >
                 <div>
@@ -17,6 +17,15 @@
                     <span class="text-sm text-white">Grupo: {{ course.group.name}}</span>
                 </div>
 
+                <div class="mt-4" v-if="course.classLink != null  || course.classLink != ''">
+                    <a
+                        :href="course.classLink"
+                        target="_blank"
+                        class="bg-white py-2 px-4 rounded-lg shadow"
+                    >
+                        Ingresar a Clase virtual
+                    </a>
+                </div>
             </header>
 
             <div class="my-4" v-html="course.html">
