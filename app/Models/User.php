3954c76b75b4a57ba95cd82d631c1c0299f687a7
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2021-08-31 14:56:00 -0600

    Fixed exersoice score

--- app/Models/User.php
+++ app/Models/User.php
@@ -86,6 +86,7 @@ class User extends Authenticatable
 
     public function gradeExercise($id, $score = 0, $comment = '', $exercise = '')
     {
+        $score = $score < 0 ? 0 : $score;
         $activity = Activity::find($id);
         $userActivity = $this->activities->where('id', $activity->id)->first();
 
