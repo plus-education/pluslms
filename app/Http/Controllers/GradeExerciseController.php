9bd277dd99d7336620f3903846d996537bd1f004
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2021-09-01 10:28:29 -0600

    Fixed exersoice score

--- app/Http/Controllers/GradeExerciseController.php
+++ app/Http/Controllers/GradeExerciseController.php
@@ -32,7 +32,6 @@ class GradeExerciseController extends Controller
             $user = User::findOrFail($request->get('studentId'));
         }
 
-        $totalScore = $totalScore < 0 ? 0 : $totalScore;
         $user->gradeExercise($request->get('activityId'), $totalScore, '', json_encode($exercise));
 
         return ['totalScore' => $totalScore];
