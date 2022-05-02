9b65870d64183d6455e5c5a9b9417be4952c6191
Roman Avery <roman@cinemanager.co.nz>
2022-05-02 18:25:57 +1200

    Fixed composer script.

--- deployment/refresh_composer.sh
+++ deployment/refresh_composer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-composer dump-autoload
+
+./deployment/uncache.sh
+
 rm -rf ./vendor/
 
 composer install --no-dev --no-interaction --prefer-dist --ignore-platform-reqs --optimize-autoloader --apcu-autoloader --ansi --no-scripts
+
+php artisan clear-compiled 
+composer dump-autoload
+php artisan optimize
 
 php artisan migrate --force
 
