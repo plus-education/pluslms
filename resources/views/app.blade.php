770f460baf9d17374fd3748050c51fad0c633f42
Roman Avery <roman@cinemanager.co.nz>
2022-05-15 11:23:42 +1200

    Added google analytics code.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -83,4 +83,15 @@
     <body class="font-sans antialiased">
         @inertia
     </body>
+    @if (env('GOOGLE_ANALYTICS_TAG'))
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_TAG') }}"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', "{{ env('GOOGLE_ANALYTICS_TAG') }}");
+        </script>
+    @endif
 </html>
